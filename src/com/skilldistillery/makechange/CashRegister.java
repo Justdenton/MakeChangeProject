38d916364e69dbeb3a74375fac8d7935bf54f057
“Justin” <jdenton.bills@gmail.com>
2024-06-21 19:24:24 -0500

    Fridaynight

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,16 +1,29 @@
 package com.skilldistillery.makechange;
 
+// 1) Calculates the amount of change returned to a customer.
+				  // Based on the purchase price & amount tendered. (trust user vs. scanning at grocery store)
+// 2) Notifies attendant (how many) of each 'type of currency' is needed to make change for the customer.  
+		// Change will be provided using combo of LARGEST bill & coin denominations as possible.
+			// *** Denominations that are NOT used will NOT be displayed ***
+																												
 public class CashRegister {
-
+	
 	public static void main(String[] args) {
 
 		
 		
 		
 		
-		
-		
+	}
+	
+	public static void askPrice() {
+		System.out.println("Hello, what is the price for that item? ");
 		
 	}
+	
+	
+	
+	
+	
 
 }
